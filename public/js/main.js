507903e8366b1f167c9931d3d09574a6dcc338ae
João Vítor <joaovitorsf7@hotmail.com>
2021-01-19 17:19:01 -0300

    opção de subnível, rem.

--- public/js/main.js
+++ public/js/main.js
@@ -1,13 +1,19 @@
 const valueForTree = document.getElementById("valueForTree");
 const treeViewer = document.getElementById("treeViewer");
+let level = 2;
 
 valueForTree.addEventListener("keypress", function (e) {
   if (e.key == "Enter") {
     if (treeViewer.textContent == "") {
-        treeViewer.textContent += `├──${valueForTree.value}\n`;
-    }
-    else {
-        treeViewer.textContent += `│  ├──${valueForTree.value}\n`
+      treeViewer.textContent += `├──${valueForTree.value}\n`;
+    } else {
+      treeViewer.textContent += `│${" ".repeat(level)}├──${
+        valueForTree.value
+      }\n`;
     }
   }
+});
+
+document.getElementById("btn-subnivel").addEventListener("click", function () {
+  level += 3;
 });
