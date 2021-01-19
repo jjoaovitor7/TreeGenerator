8ffe9c4ab9e2bd841b89009c95e0c00b65483539
João Vítor <joaovitorsf7@hotmail.com>
2021-01-19 18:21:47 -0300

    subtractsubnível e screenshot.

--- public/js/main.js
+++ public/js/main.js
@@ -11,10 +11,15 @@ document.getElementById("btn-nofilho").addEventListener("click", function () {
   treeViewer.innerHTML += `│${" ".repeat(level)}├──${valueForTree.value}\n`;
 });
 
-document.getElementById("btn-subnivel").addEventListener("click", function () {
+document.getElementById("btn-addsubnivel").addEventListener("click", function () {
   level += 3;
   treeViewer.innerHTML += `│${" ".repeat(level)}└──${valueForTree.value}\n`;
   level += 3;
+});
+
+document.getElementById("btn-subtractsubnivel").addEventListener("click", function () {
+  level -= 3;
+  treeViewer.innerHTML += `│${" ".repeat(level)}└──${valueForTree.value}\n`;
 });
 
 document.getElementById("btn-nofinal").addEventListener("click", function () {
