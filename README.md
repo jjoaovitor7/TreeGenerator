eb62d94aee1e5175d25a024d33ae135a39019c60
João Vítor <joaovitorsf7@hotmail.com>
2021-01-19 19:59:41 -0300

    fixes e screenshot atualizada.

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # TreeGenerator
 
 <p align="center">
-  <img src="./screenshots/1.png">
+  <img src="./screenshots/2.png">
 </p>
 
 <p align="center">
--- public/js/main.js
+++ public/js/main.js
@@ -3,25 +3,25 @@ const treeViewer = document.getElementById("treeViewer");
 let level = 2;
 
 document.getElementById("btn-noprincipal").addEventListener("click", function () {
-    treeViewer.innerHTML += `├──${valueForTree.value}\n`;
+    treeViewer.innerHTML += `${valueForTree.value}\n`;
     level = 2;
+    valueForTree.value = "";
 });
 
 document.getElementById("btn-nofilho").addEventListener("click", function () {
   treeViewer.innerHTML += `│${" ".repeat(level)}├──${valueForTree.value}\n`;
+  valueForTree.value = "";
 });
 
 document.getElementById("btn-addsubnivel").addEventListener("click", function () {
-  level += 3;
-  treeViewer.innerHTML += `│${" ".repeat(level)}└──${valueForTree.value}\n`;
   level += 3;
 });
 
 document.getElementById("btn-subtractsubnivel").addEventListener("click", function () {
   level -= 3;
-  treeViewer.innerHTML += `│${" ".repeat(level)}└──${valueForTree.value}\n`;
 });
 
 document.getElementById("btn-nofinal").addEventListener("click", function () {
   treeViewer.innerHTML += `│${" ".repeat(level)}└──${valueForTree.value}\n`;
+  valueForTree.value = "";
 });
