31dd0c0fa7a1062badc8c0550681917a3c83a365
João Vítor <joaovitorsf7@hotmail.com>
2021-01-19 17:50:36 -0300

    botões Nó FIlho e Nó Principal.

--- public/js/main.js
+++ public/js/main.js
@@ -2,14 +2,13 @@ const valueForTree = document.getElementById("valueForTree");
 const treeViewer = document.getElementById("treeViewer");
 let level = 2;
 
-valueForTree.addEventListener("keypress", function (e) {
-  if (e.key == "Enter") {
-    if (treeViewer.textContent == "") {
-      treeViewer.innerHTML += `├──${valueForTree.value}\n`;
-    } else {
-      treeViewer.innerHTML += `│${" ".repeat(level)}├──${valueForTree.value}\n`;
-    }
-  }
+document.getElementById("btn-noprincipal").addEventListener("click", function () {
+    treeViewer.innerHTML += `├──${valueForTree.value}\n`;
+    level = 2;
+});
+
+document.getElementById("btn-nofilho").addEventListener("click", function () {
+  treeViewer.innerHTML += `│${" ".repeat(level)}├──${valueForTree.value}\n`;
 });
 
 document.getElementById("btn-subnivel").addEventListener("click", function () {
