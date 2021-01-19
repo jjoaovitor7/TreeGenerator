4a95fa10dfe26f197fccabb920d4cd0f78525111
João Vítor <joaovitorsf7@hotmail.com>
2021-01-19 17:29:17 -0300

    fix do subnível e nó final.

--- public/js/main.js
+++ public/js/main.js
@@ -5,15 +5,19 @@ let level = 2;
 valueForTree.addEventListener("keypress", function (e) {
   if (e.key == "Enter") {
     if (treeViewer.textContent == "") {
-      treeViewer.textContent += `├──${valueForTree.value}\n`;
+      treeViewer.innerHTML += `├──${valueForTree.value}\n`;
     } else {
-      treeViewer.textContent += `│${" ".repeat(level)}├──${
-        valueForTree.value
-      }\n`;
+      treeViewer.innerHTML += `│${" ".repeat(level)}├──${valueForTree.value}\n`;
     }
   }
 });
 
 document.getElementById("btn-subnivel").addEventListener("click", function () {
   level += 3;
+  treeViewer.innerHTML += `│${" ".repeat(level)}└──${valueForTree.value}\n`;
+  level += 3;
+});
+
+document.getElementById("btn-nofinal").addEventListener("click", function () {
+  treeViewer.innerHTML += `│${" ".repeat(level)}└──${valueForTree.value}\n`;
 });
