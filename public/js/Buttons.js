d72d9ee2cb232b8992738dba631c56daa49a1fe3
João Vítor <48962164+jjoaovitor7@users.noreply.github.com>
2024-02-16 14:29:01 -0300

    funcionalidade de exportar a saída p/ .tree e alguns ajustes.

--- public/js/Buttons.js
+++ public/js/Buttons.js
@@ -4,32 +4,34 @@ class Buttons {
   }
 
   btnSubtractNivel(levelObject) {
-    levelObject.level -= 3;
+    if (levelObject.level >= 3) {
+      levelObject.level -= 3;
+    }
   }
 
   btnNoPrincipal(treeViewer, levelObject, valueForTree) {
-    treeViewer.innerHTML += `${valueForTree.value}\n`;
-    levelObject.level = 2;
+    treeViewer.value += `${valueForTree.value}\n`;
+    levelObject.level = 0;
     valueForTree.value = "";
   }
 
   btnNoFilho(treeViewer, levelObject, valueForTree) {
-    treeViewer.innerHTML += `│${" ".repeat(levelObject.level)}├──${
-      valueForTree.value
-    }\n`;
+    treeViewer.value += `│${" "
+      .repeat(levelObject.level)}├──${valueForTree.value
+      }\n`;
     valueForTree.value = "";
   }
 
   btnNoFinal(treeViewer, levelObject, valueForTree) {
-    treeViewer.innerHTML += `│${" ".repeat(levelObject.level)}└──${
-      valueForTree.value
-    }\n`;
+    treeViewer.value += `│${" "
+      .repeat(levelObject.level)}└──${valueForTree.value
+      }\n`;
     valueForTree.value = "";
   }
 
   btnLimpar(levelObject, treeViewer) {
     levelObject.level = 2;
-    treeViewer.innerHTML = "";
+    treeViewer.value = "";
   }
 
   btnPorArquivo(treeViewer) {
@@ -43,25 +45,49 @@ class Buttons {
         for (let i = text.length; i >= 1; i--) {
           while (text.includes(">".repeat(i) + ".")) {
             if (i == 0) {
-              text = text.replace(">".repeat(i) + ".", `│${" ".repeat(2)}└──`);
+              text = text
+                .replace(">".repeat(i) + ".", `│${" ".repeat(1)}└──`);
             }
             else {
-              text = text.replace(">".repeat(i) + ".", `│${" ".repeat(i*2.5)}└──`);
+              text = text
+                .replace(">".repeat(i) + ".", `│${" ".repeat(i * 2.5)}└──`);
             }
           }
 
           while (text.includes(">".repeat(i))) {
             if (i == 0) {
-              text = text.replace(">".repeat(i), `│${" ".repeat(2)}├──`);
+              text = text.
+                replace(">".repeat(i), `│${" ".repeat(2)}├──`);
             }
             else {
-              text = text.replace(">".repeat(i), `│${" ".repeat(i*2.5)}├──`);
+              text = text
+                .replace(">".repeat(i), `│${" ".repeat(i * 2.5)}├──`);
             }
           }
         }
-        treeViewer.innerHTML = text;
+        treeViewer.value = text;
       });
     });
+  }
+
+  btnExportar(treeViewer) {
+    var content = treeViewer.value.replaceAll(/\n/g, '')
+      .replaceAll(/\s\s\s/g, '>')
+      .replaceAll(/\s/g, '')
+      .replaceAll("│", "\n")
+      .replaceAll("├──", ">")
+      .replaceAll("└──", ">.");
+
+    var blob = new Blob([content], { type: 'text/plain' });
+
+    var url = URL.createObjectURL(blob);
+
+    var link = document.createElement('a');
+    link.href = url;
+    link.download = `${crypto.getRandomValues(new Uint8Array(4)).join("")}.tree`
+    link.click();
+
+    URL.revokeObjectURL(url);
   }
 }
 
--- public/js/main.js
+++ public/js/main.js
@@ -1,44 +1,66 @@
 import Buttons from "./Buttons.js";
 
 const valueForTree = document.getElementById("valueForTree");
-const treeViewer   = document.getElementById("treeViewer");
+const treeViewer = document.getElementById("treeViewer");
 
 let levelObject = {
-  level: 2
+  level: 0
 };
 
-const btnAddSubNivel   = document.getElementById("btn-addsubnivel");
+const textLevelObject = document.querySelector("#level-object span");
+const btnAddSubNivel = document.getElementById("btn-addsubnivel");
 const btnSubtractNivel = document.getElementById("btn-subtractsubnivel");
-const btnNoPrincipal   = document.getElementById("btn-noprincipal");
-const btnNoFilho       = document.getElementById("btn-nofilho");
-const btnNoFinal       = document.getElementById("btn-nofinal");
-const btnLimpar        = document.getElementById("btn-limpar");
-const btnPorArquivo    = document.getElementById("btn-fromfile");
+const btnNoPrincipal = document.getElementById("btn-noprincipal");
+const btnNoFilho = document.getElementById("btn-nofilho");
+const btnNoFinal = document.getElementById("btn-nofinal");
+const btnLimpar = document.getElementById("btn-limpar");
+const btnPorArquivo = document.getElementById("btn-fromfile");
+const btnExportar = document.getElementById("btn-export");
+
+textLevelObject.textContent = levelObject.level;
 
 btnAddSubNivel.addEventListener("click", function () {
   Buttons.btnAddSubNivel(levelObject);
+  textLevelObject.textContent = parseInt(levelObject.level / 3);
 });
 
 btnSubtractNivel.addEventListener("click", function () {
   Buttons.btnSubtractNivel(levelObject);
+  textLevelObject.textContent = parseInt(levelObject.level / 3);
 });
 
 btnNoPrincipal.addEventListener("click", function () {
-  Buttons.btnNoPrincipal(treeViewer, levelObject, valueForTree);
+  if (valueForTree.value.trim() != "") {
+    Buttons.btnNoPrincipal(treeViewer, levelObject, valueForTree);
+    textLevelObject.textContent = parseInt(levelObject.level / 3);
+  }
 });
 
 btnNoFilho.addEventListener("click", function () {
-  Buttons.btnNoFilho(treeViewer, levelObject, valueForTree);
+  if (valueForTree.value.trim() != "") {
+    Buttons.btnNoFilho(treeViewer, levelObject, valueForTree);
+    textLevelObject.textContent = parseInt(levelObject.level / 3);
+  }
 });
 
 btnNoFinal.addEventListener("click", function () {
-  Buttons.btnNoFinal(treeViewer, levelObject, valueForTree);
+  if (valueForTree.value.trim() != "") {
+    Buttons.btnNoFinal(treeViewer, levelObject, valueForTree);
+    textLevelObject.textContent = parseInt(levelObject.level / 3);
+  }
 });
 
 btnLimpar.addEventListener("click", function () {
   Buttons.btnLimpar(levelObject, treeViewer);
+  textLevelObject.textContent = parseInt(levelObject.level / 3);
 });
 
 btnPorArquivo.addEventListener("click", function () {
   Buttons.btnPorArquivo(treeViewer);
+  textLevelObject.textContent = parseInt(levelObject.level / 3);
+});
+
+btnExportar.addEventListener('click', function () {
+  Buttons.btnExportar(treeViewer);
+  textLevelObject.textContent = parseInt(levelObject.level / 3);
 });
